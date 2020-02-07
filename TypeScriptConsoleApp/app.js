455d7f69ca8e021a30572dff3d5bad177c325eb8
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-02-07 15:38:55 +0300

    Modified User class

---
+++ TypeScriptConsoleApp/app.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(_name, _surname, _age) {
+        this.name = _name;
+        this.age = _age;
+        this.surname = _surname;
+    }
+}
+var user = new User("Ivan", "Ivanov", 29);
+console.log("Name: ", user.name, " Age: ", user.age);
+//# sourceMappingURL=app.js.map
