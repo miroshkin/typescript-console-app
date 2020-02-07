d490faa876de96ab290ba0a49663d95b169d5dcd
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-02-07 13:55:35 +0300

    Added user class

--- TypeScriptConsoleApp/app.ts
+++ TypeScriptConsoleApp/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello world');
+class User {
+    name: string;
+    surname: string;
+    age: number;
+    constructor(_name: string,_surname: string, _age: number) {
+
+        this.name = _name;
+        this.age = _age;
+        this.surname = _surname;
+    }
+}
+var user: User = new User("Ivan", "Ivanov", 29);
+console.log("Name: ", user.name, " Age: ", user.age);
